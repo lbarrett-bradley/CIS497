6ac3cceabec44373b45707fbc85400e10b4d85e9
lbarrettCIS497 <lbarrett@mail.bradley.edu>
2020-09-13 23:04:55 -0500

    Created and Completed Challenge 2

---
+++ UnityProjects/Challenge 2/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    private DisplayScore displayScoreScript;
+
+    void Start()
+    {
+        displayScoreScript = GameObject.FindGameObjectWithTag("DisplayScoreText").GetComponent<DisplayScore>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        displayScoreScript.score++;
+        Destroy(gameObject);
+    }
+}
---
+++ UnityProjects/Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    private HealthSystem healthSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthSystem = GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>();
+
+        //InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        StartCoroutine(SpawnRandomBallWithCoroutine());
+    }
+
+    IEnumerator SpawnRandomBallWithCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while(!healthSystem.gameOver)
+        {
+            SpawnRandomBall();
+
+            float randomDelay = Random.Range(3, 5);
+
+            yield return new WaitForSeconds(randomDelay);
+        }
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        int index = Random.Range(0, 2);
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[index], spawnPos, ballPrefabs[0].transform.rotation);
+    }
+
+}
