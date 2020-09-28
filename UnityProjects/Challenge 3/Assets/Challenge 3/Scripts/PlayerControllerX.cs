f4a17f1d6438f6d81baca15712cad1f46510297f
lbarrettCIS497 <lbarrett@mail.bradley.edu>
2020-09-27 23:34:20 -0500

    Created and completed Challenge 3

---
+++ UnityProjects/Challenge 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Liam Barrett
+ * Challenge 3
+ * Allows player to control player object
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip bounceSound;
+
+    public Text scoreText;
+    private int score;
+
+    private bool won = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+        scoreText.text = "Score: 0";
+        won = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update the score text as score increases
+        scoreText.text = "Score: " + score;
+
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+
+        //shows game over text when the player wins or loses
+        if (gameOver)
+        {
+            if (won)
+            {
+                scoreText.text = "You Win!\nPress R to Try Again!";
+            }
+            else
+            {
+                scoreText.text = "You Lose!\nPress R to Try Again!";
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && gameOver)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb") && !gameOver)
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } 
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money") && !gameOver)
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+
+            score++;
+            if (score >= 20)
+            {
+                gameOver = true;
+                won = true;
+            }
+        }
+        
+        //if player collides with the ground, bounce
+        else if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+
+            //play bounce sound
+            playerAudio.PlayOneShot(bounceSound, 1.0f);
+        }
+
+    }
+
+}
